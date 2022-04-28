0f4fd9c0dc815e143541044f4c33726c7800dd1e
김스윔 <65752458+eminentecero@users.noreply.github.com>
2022-04-28 23:35:20 +0900

    [Silver II] Title: 최소 힙, Time: 24 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+// 연산의 개수
+int N;
+int num=-1;
+
+// 힙
+priority_queue<int, vector<int>, greater<int>> heap;
+
+int main() {
+	scanf("%d", &N);
+	//cin >> N;
+	for (int i = 0; i < N; i++) {
+		//cin >> num;
+		scanf("%d", &num);
+		if (num != 0) {
+			// 아니면 그냥 heap에 값을 삽입
+			heap.push(num);
+		}
+		else if (num == 0 && !heap.empty()) {
+			// 최소값
+			//cout << heap.top() << endl;
+			printf("%d \n", heap.top());
+			heap.pop();
+		}
+		else {
+			printf("0 \n");
+			//cout << "0" << endl;
+		}
+	}
+}
